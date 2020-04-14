aa864029f72bfe3972073b95bdd0b4f47ce280e0
Jan Berdel <jan.berdel@glueckkanja.com>
2020-04-14 13:39:19 +0200

    Fix typo v0.1.12

--- SlimGraph/SlimGraphConstants.cs
+++ SlimGraph/SlimGraphConstants.cs
@@ -2,7 +2,7 @@
 {
     public static class SlimGraphConstants
     {
-        public const string EndpointV1 = "https://graph.microsoft.com/v1/";
+        public const string EndpointV1 = "https://graph.microsoft.com/v1.0/";
         public const string EndpointBeta = "https://graph.microsoft.com/beta/";
 
         public const string ScopeDefault = "https://graph.microsoft.com/.default";
