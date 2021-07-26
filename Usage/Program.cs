83ca2574e90feed6e7388c39cda8a3ced1459c10
Jan Berdel <jan.berdel@glueckkanja-gab.com>
2021-07-26 12:46:41 +0200

    Clean: Usage

--- Usage/Program.cs
+++ Usage/Program.cs
@@ -35,70 +35,70 @@ namespace Usage
                     .AddConsole();
             });
 
-            var tenant = new AzureTenant(Configuration.GetValue<string>("Tenant"));
             var clientCredentials = new AzureClientCredentials();
             Configuration.GetSection("AzureAD").Bind(clientCredentials);
 
+            services.AddHttpClient();
             services.AddMemoryCache();
-            services.AddTransient<IAuthenticationProvider>(sp => new AzureAuthenticationClient(clientCredentials, sp.GetRequiredService<IHttpClientFactory>().CreateClient(nameof(AzureAuthenticationClient)), sp.GetService<IMemoryCache>()));
+            services.AddSingleton<IAuthenticationProvider>(sp => new AzureAuthenticationClient(clientCredentials, sp.GetRequiredService<IHttpClientFactory>().CreateClient(nameof(AzureAuthenticationClient)), sp.GetService<IMemoryCache>()));
             services.AddHttpClient<ISlimGraphClient, SlimGraphClient>(client => client.BaseAddress = new Uri(SlimGraphConstants.EndpointBeta)).AddTransientHttpErrorPolicy(p => p.WaitAndRetryAsync(3, _ => TimeSpan.FromMilliseconds(500)));
 
-            var container = services.BuildServiceProvider();
+            using var container = services.BuildServiceProvider();
+            using var serviceScope = container.CreateScope();
 
-            using (var scope = container.CreateScope())
+            var tenant = new AzureTenant(Configuration.GetValue<string>("Tenant"));
+
+            var client = serviceScope.ServiceProvider.GetRequiredService<ISlimGraphClient>();
+            var logger = serviceScope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+            var count = 0;
+            await foreach (var item in client.Users.GetUsersAsync(tenant, new ListRequestOptions { Top = 10, OrderBy = "displayName desc" }))
             {
-                var client = scope.ServiceProvider.GetRequiredService<ISlimGraphClient>();
-                var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+                // Since $top = 10 is set, each page will have at most 10 items. This means GetUsersAsync will execute two HTTP calls.
+                logger.LogInformation("Got {fn} item: {id}", nameof(client.Users.GetUsersAsync), item.GetProperty("id").GetGuid());
+                if (++count >= 20) break;
+            }
 
-                var count = 0;
-                await foreach (var item in client.Users.GetUsersAsync(tenant, new ListRequestOptions { Top = 10, OrderBy = "displayName desc" }))
+            count = 0;
+            await foreach (var item in client.Groups.GetGroupsAsync(tenant, new ListRequestOptions { Top = 20 }))
+            {
+                logger.LogInformation("Got {fn} item: {id}", nameof(client.Groups.GetGroupsAsync), item.GetProperty("id").GetGuid());
+                if (++count >= 20) break;
+            }
+
+            var cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (sender, e) =>
+            {
+                logger.LogInformation("Aborting...");
+                e.Cancel = true;
+                cts.Cancel();
+            };
+
+            try
+            {
+                var deltaInitial = await client.Users.GetUsersDeltaAsync(tenant, new DeltaRequestOptions { PreferMinimal = true }, cancellationToken: cts.Token);
+                var deltaLink = deltaInitial.DeltaLink;
+
+                logger.LogInformation("Got users initial state: {n} items", deltaInitial.Items.Count);
+
+                while (!string.IsNullOrEmpty(deltaLink) && !cts.IsCancellationRequested)
                 {
-                    // Since $top = 10 is set, each page will have at most 10 items. This means GetUsersAsync will execute two HTTP calls.
-                    logger.LogInformation("Got {fn} item: {id}", nameof(client.Users.GetUsersAsync), item.GetProperty("id").GetGuid());
-                    if (++count >= 20) break;
-                }
+                    Console.ReadLine();
 
-                count = 0;
-                await foreach (var item in client.Groups.GetGroupsAsync(tenant, new ListRequestOptions { Top = 20 }))
-                {
-                    logger.LogInformation("Got {fn} item: {id}", nameof(client.Groups.GetGroupsAsync), item.GetProperty("id").GetGuid());
-                    if (++count >= 20) break;
-                }
+                    var deltaChanges = await client.Users.GetUsersDeltaChangeAsync(tenant, deltaLink, new DeltaRequestOptions { PreferMinimal = true }, cancellationToken: cts.Token);
+                    logger.LogInformation("Got users changes: {n} items", deltaChanges.Items.Count);
 
-                var cts = new CancellationTokenSource();
-                Console.CancelKeyPress += (sender, e) =>
-                {
-                    logger.LogInformation("Aborting...");
-                    e.Cancel = true;
-                    cts.Cancel();
-                };
-
-                try
-                {
-                    var deltaInitial = await client.Users.GetUsersDeltaAsync(tenant, new DeltaRequestOptions { PreferMinimal = true }, cancellationToken: cts.Token);
-                    var deltaLink = deltaInitial.DeltaLink;
-
-                    logger.LogInformation("Got users initial state: {n} items", deltaInitial.Items.Count);
-
-                    while (!string.IsNullOrEmpty(deltaLink) && !cts.IsCancellationRequested)
+                    foreach (var item in deltaChanges.Items)
                     {
-                        Console.ReadLine();
-
-                        var deltaChanges = await client.Users.GetUsersDeltaChangeAsync(tenant, deltaLink, new DeltaRequestOptions { PreferMinimal = true }, cancellationToken: cts.Token);
-                        logger.LogInformation("Got users changes: {n} items", deltaChanges.Items.Count);
-
-                        foreach (var item in deltaChanges.Items)
-                        {
-                            if (cts.IsCancellationRequested) return;
-                            logger.LogInformation("Got user change: {item}", item.GetRawText());
-                        }
-
-                        deltaLink = deltaChanges.DeltaLink;
+                        if (cts.IsCancellationRequested) return;
+                        logger.LogInformation("Got user change: {item}", item.GetRawText());
                     }
+
+                    deltaLink = deltaChanges.DeltaLink;
                 }
-                catch (OperationCanceledException)
-                {
-                }
+            }
+            catch (OperationCanceledException)
+            {
             }
         }
     }
