a6b1ca1d19be519c12035409778235a4639e6591
Jan Berdel <jan.berdel@glueckkanja-gab.com>
2022-02-23 12:44:58 +0100

    Revert "Fix organization endpoints"
    This reverts commit 1dac1b3517d491d79bbc8b48fb9edfd2ac588038.

--- SlimLib.Microsoft.Graph/ClientInterfaces/ISlimGraphOrganizationsClient.cs
+++ SlimLib.Microsoft.Graph/ClientInterfaces/ISlimGraphOrganizationsClient.cs
@@ -1,4 +1,6 @@
 ﻿using SlimLib.Auth.Azure;
+using System;
+using System.Collections.Generic;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
@@ -7,6 +9,8 @@ namespace SlimLib.Microsoft.Graph
 {
     public interface ISlimGraphOrganizationsClient
     {
-        Task<JsonElement> GetOrganizationAsync(IAzureTenant tenant, ScalarRequestOptions? options = default, CancellationToken cancellationToken = default);
+        Task<JsonElement> GetOrganizationAsync(IAzureTenant tenant, Guid organizationID, ScalarRequestOptions? options = default, CancellationToken cancellationToken = default);
+
+        IAsyncEnumerable<JsonElement> GetOrganizationsAsync(IAzureTenant tenant, ListRequestOptions? options = default, CancellationToken cancellationToken = default);
     }
 }
