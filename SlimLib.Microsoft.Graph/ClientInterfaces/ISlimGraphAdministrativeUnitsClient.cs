898f644eef10ba97f1de6a5ac35c6976fb484d0a
Jan Berdel <jan.berdel@glueckkanja-gab.com>
2023-06-27 11:44:19 +0200

    Add GetMembersAsync

--- SlimLib.Microsoft.Graph/ClientInterfaces/ISlimGraphAdministrativeUnitsClient.cs
+++ SlimLib.Microsoft.Graph/ClientInterfaces/ISlimGraphAdministrativeUnitsClient.cs
@@ -1,5 +1,6 @@
 ﻿using SlimLib.Auth.Azure;
 using System;
+using System.Collections.Generic;
 using System.Text.Json;
 using System.Text.Json.Nodes;
 using System.Threading;
@@ -10,5 +11,8 @@ namespace SlimLib.Microsoft.Graph
     public interface ISlimGraphAdministrativeUnitsClient
     {
         Task<JsonElement> AddMemberAsync(IAzureTenant tenant, Guid adminUnitID, JsonObject data, InvokeRequestOptions? options = default, CancellationToken cancellationToken = default);
+
+        IAsyncEnumerable<JsonElement> GetMembersAsync(IAzureTenant tenant, Guid adminUnitID, ListRequestOptions? options = default, CancellationToken cancellationToken = default);
+        IAsyncEnumerable<JsonElement> GetMembersAsync(IAzureTenant tenant, Guid adminUnitID, string type, ListRequestOptions? options = default, CancellationToken cancellationToken = default);
     }
 }
--- SlimLib.Microsoft.Graph/Impl/SlimGraphClientImpl.AdministrativeUnits.cs
+++ SlimLib.Microsoft.Graph/Impl/SlimGraphClientImpl.AdministrativeUnits.cs
@@ -1,5 +1,7 @@
 ﻿using SlimLib.Auth.Azure;
 using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text.Json;
 using System.Text.Json.Nodes;
 using System.Threading;
@@ -14,6 +16,32 @@ namespace SlimLib.Microsoft.Graph
             var link = BuildLink(options, $"directory/administrativeUnits/{adminUnitID}/members");
 
             return await PostAsync(tenant, JsonSerializer.SerializeToUtf8Bytes(data), link, new RequestHeaderOptions(), cancellationToken).ConfigureAwait(false);
+        }
+
+        async IAsyncEnumerable<JsonElement> ISlimGraphAdministrativeUnitsClient.GetMembersAsync(IAzureTenant tenant, Guid adminUnitID, ListRequestOptions? options, [EnumeratorCancellation] CancellationToken cancellationToken)
+        {
+            var nextLink = BuildLink(options, $"directory/administrativeUnits/{adminUnitID}/members");
+
+            await foreach (var item in GetArrayAsync(tenant, nextLink, options, cancellationToken))
+            {
+                if (cancellationToken.IsCancellationRequested)
+                    yield break;
+
+                yield return item;
+            }
+        }
+
+        async IAsyncEnumerable<JsonElement> ISlimGraphAdministrativeUnitsClient.GetMembersAsync(IAzureTenant tenant, Guid adminUnitID, string type, ListRequestOptions? options, [EnumeratorCancellation] CancellationToken cancellationToken)
+        {
+            var nextLink = BuildLink(options, $"directory/administrativeUnits/{adminUnitID}/members/{type}");
+
+            await foreach (var item in GetArrayAsync(tenant, nextLink, options, cancellationToken))
+            {
+                if (cancellationToken.IsCancellationRequested)
+                    yield break;
+
+                yield return item;
+            }
         }
     }
 }
